25202db7eaafcb9d9b39878088c736e6b07e16e3
Andy Armstrong <andya@edx.org>
2017-02-27 17:36:17 -0500

    Small tweaks to the FragmentView API

--- web_fragments/__init__.py
+++ web_fragments/__init__.py
@@ -4,6 +4,6 @@ Web fragments.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 default_app_config = 'web_fragments.apps.WebFragmentsConfig'  # pylint: disable=invalid-name
